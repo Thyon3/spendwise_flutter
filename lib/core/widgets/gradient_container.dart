8e2e915213f8e27d1e5b0919c20b92ec5ee98b21
Thyon3 <asnakemengesha80@gmail.com>
2026-03-05 19:10:48 -0500

    feat: add gradient container widgets for visual effects

---
+++ lib/core/widgets/gradient_container.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  final Widget child;
+  final List<Color> colors;
+  final AlignmentGeometry begin;
+  final AlignmentGeometry end;
+  final BorderRadius? borderRadius;
+  final EdgeInsetsGeometry? padding;
+
+  const GradientContainer({
+    super.key,
+    required this.child,
+    required this.colors,
+    this.begin = Alignment.topLeft,
+    this.end = Alignment.bottomRight,
+    this.borderRadius,
+    this.padding,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: padding,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: colors,
+          begin: begin,
+          end: end,
+        ),
+        borderRadius: borderRadius,
+      ),
+      child: child,
+    );
+  }
+}
+
+class AnimatedGradientContainer extends StatefulWidget {
+  final Widget child;
+  final List<Color> colors;
+  final Duration duration;
+  final BorderRadius? borderRadius;
+  final EdgeInsetsGeometry? padding;
+
+  const AnimatedGradientContainer({
+    super.key,
+    required this.child,
+    required this.colors,
+    this.duration = const Duration(seconds: 3),
+    this.borderRadius,
+    this.padding,
+  });
+
+  @override
+  State<AnimatedGradientContainer> createState() =>
+      _AnimatedGradientContainerState();
+}
+
+class _AnimatedGradientContainerState extends State<AnimatedGradientContainer>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: widget.duration,
+    )..repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return Container(
+          padding: widget.padding,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: widget.colors,
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              stops: [
+                _controller.value,
+                1.0,
+              ],
+            ),
+            borderRadius: widget.borderRadius,
+          ),
+          child: widget.child,
+        );
+      },
+    );
+  }
+}
