9f6429d89f4349f32cd43cbfb58921023edf1b64
Thyon3 <asnakemengesha80@gmail.com>
2026-03-06 08:04:31 -0500

    feat: add circular icon button widget

---
+++ lib/core/widgets/circular_icon_button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class CircularIconButton extends StatelessWidget {
+  final IconData icon;
+  final VoidCallback onPressed;
+  final Color? backgroundColor;
+  final Color? iconColor;
+  final double size;
+
+  const CircularIconButton({
+    super.key,
+    required this.icon,
+    required this.onPressed,
+    this.backgroundColor,
+    this.iconColor,
+    this.size = 48,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size,
+      height: size,
+      decoration: BoxDecoration(
+        color: backgroundColor ?? Theme.of(context).primaryColor,
+        shape: BoxShape.circle,
+      ),
+      child: IconButton(
+        icon: Icon(icon, color: iconColor ?? Colors.white),
+        onPressed: onPressed,
+      ),
+    );
+  }
+}
